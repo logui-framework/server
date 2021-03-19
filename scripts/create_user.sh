d8fb185a29e2ab03711ad7c0cf99f67b88f9aa44
David Maxwell <maxwelld90@gmail.com>
2021-03-19 22:00:16 +0000

    Updated scripts

--- scripts/create_user.sh
+++ scripts/create_user.sh
@@ -3,10 +3,10 @@
 #
 # LogUI Server User Creation Script
 # Creates a new user account for the LogUI server control app.
-# Assumes that the LogUI server docker instance is running, and that the HTTP worker is at the standard name.
+# Assumes that the LogUI server docker instance is running, and that the HTTP worker is at the expected container name.
 #
 # Author: David Maxwell
 # Date: 2021-03-19
 #
 
-docker exec -it server_http-worker_1 python manage.py createsuperuser --username=$1
+docker exec -it logui_http-worker_1 python manage.py createsuperuser --username=$1
