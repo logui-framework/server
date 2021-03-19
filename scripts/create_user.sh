262705d1f93a303d90d1d780cce7b3e3ff244911
David Maxwell <maxwelld90@gmail.com>
2021-03-19 21:44:48 +0000

    Updated scripts and environment setup

--- scripts/create_user.sh
+++ scripts/create_user.sh
@@ -1,3 +1,12 @@
-#!/bin/bash
+#!/bin/sh
+
+#
+# LogUI Server User Creation Script
+# Creates a new user account for the LogUI server control app.
+# Assumes that the LogUI server docker instance is running, and that the HTTP worker is at the standard name.
+#
+# Author: David Maxwell
+# Date: 2021-03-19
+#
 
 docker exec -it server_http-worker_1 python manage.py createsuperuser --username=$1
--- scripts/firstrun.sh
+++ scripts/firstrun.sh
@@ -1,12 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
 #
 # LogUI Server First Run Script
-# Initialises the LogUI Server to be ready for the manipulation of the database and MongoDB instance.
+# Creates an environment file, .env, for the Docker instance to use.
 #
 # Author: David Maxwell
-# Date: 2021-01-25
+# Date: 2021-03-19
 #
 
-# 1. create the database
-# 2. create the superuser!
+HOSTNAME=$(uname -n)
+SECRET_KEY=$(LC_CTYPE=C tr -dc A-Za-z0-9_\!\@\#\$\%\^\*\(\)-+= < /dev/urandom | head -c 50 | xargs)
+DB_PASSWORD=$(LC_CTYPE=C tr -dc A-Za-z0-9 < /dev/urandom | head -c 8 | xargs)
+
+cp ../.env.example ../.env
+awk -v hostname="$HOSTNAME" -v secret_key="$SECRET_KEY" -v password="$DB_PASSWORD" '{gsub("<<HOSTNAME>>", hostname, $0); gsub("<<KEY>>", secret_key, $0); gsub("<<PASSWORD>>", password, $0); print}' ../.env > ../.tmp && mv -f ../.tmp ../.env
