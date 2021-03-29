f41b5b91b77918cd68cc35528295ee9b9628f19c
David Maxwell <maxwelld90@gmail.com>
2021-03-29 17:15:18 +0100

    Updated reference in control app

--- app/logui-client/common/conferenceProceedings.js
+++ app/logui-client/common/conferenceProceedings.js
@@ -8,9 +8,9 @@ class ConferenceProceedings extends React.Component {
                 @inproceedings{"{"}maxwell2021logui,<br />
                 &nbsp;&nbsp;&nbsp;&nbsp;author = {"{"}Maxwell, David and Hauff, Claudia{"}"},<br />
                 &nbsp;&nbsp;&nbsp;&nbsp;title = {"{"}LogUI: Contemporary Logging Infrastructure for Web-Based Experiments{"}"},<br />
-                &nbsp;&nbsp;&nbsp;&nbsp;booktitle = {"{"}Proceedings of the 43\textsuperscript{"{"}rd{"}"} ECIR{"}"},<br />
+                &nbsp;&nbsp;&nbsp;&nbsp;booktitle = {"{"}Advanced in Information Retrieval (Proc. ECIR){"}"},<br />
                 &nbsp;&nbsp;&nbsp;&nbsp;year = {"{"}2021{"}"},<br />
-                &nbsp;&nbsp;&nbsp;&nbsp;pages = {"{"}???-???{"}"}<br />
+                &nbsp;&nbsp;&nbsp;&nbsp;pages = {"{"}525--530{"}"}<br />
                 {"}"}
             </code>
         );
