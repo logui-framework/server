22face80d89061652ae6105643da86741fb8d585
David Maxwell <maxwelld90@gmail.com>
2022-02-03 14:42:47 +0000

    Updated endpoint to work with 0.5.4

--- worker/logui_apps/websocket/consumers/endpoint.py
+++ worker/logui_apps/websocket/consumers/endpoint.py
@@ -8,7 +8,7 @@ from urllib.parse import urlparse
 from django.core import signing
 from datetime import datetime
 
-SUPPORTED_CLIENTS = ['0.5.1', '0.5.2', '0.5.3']
+SUPPORTED_CLIENTS = ['0.5.1', '0.5.2', '0.5.3', '0.5.4']
 KNOWN_REQUEST_TYPES = ['handshake', 'closedown', 'logEvents']
 BAD_REQUEST_LIMIT = 3
 
